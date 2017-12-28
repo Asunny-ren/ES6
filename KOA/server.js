fe58ad889c186e39370b6e05255ca21a2e58b277
A-sunny-ren <1398488165@qq.com>
2017-12-28 15:04:23 +0800

    koa使用generator
    koav3中已经抛弃了generator，利用更给力的东西

---
+++ KOA/server.js
@@ -0,0 +1,17 @@
+const koa = require('koa');
+const mysql = require('koa-mysql');
+
+let db = mysql.createPool({host: 'localhost', user: '******', password: '******', database: 'myDatabase'});
+
+let server = new koa();
+
+
+// v3版本已经抛抛弃了generator
+
+server.use(function * () {
+    let data = yield db.query(`SELECT * FROM mytable`);
+
+    this.body = data;
+})
+
+server.listen('8080');
