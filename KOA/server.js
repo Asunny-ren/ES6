0e10bdb0ce13f0a24c2821747181441e429a509e
A-sunny-ren <1398488165@qq.com>
2018-01-10 08:58:38 +0800

    a

--- KOA/server.js
+++ KOA/server.js
@@ -1,14 +1,19 @@
 const koa = require('koa');
 const mysql = require('koa-mysql');
 
-let db = mysql.createPool({host: 'localhost', user: '******', password: '******', database: 'myDatabase'});
+let db = mysql.createPool({
+    host: 'localhost',
+    user: '******',
+    password: '******',
+    database: 'myDatabase'
+});
 
 let server = new koa();
 
 
 // v3版本已经抛抛弃了generator
 
-server.use(function * () {
+server.use(function* () {
     let data = yield db.query(`SELECT * FROM mytable`);
 
     this.body = data;
